2915b3370a589604d5582d12d38e4a492529e5a4
João Gabriel Macedo <jgabreumacedo@gmail.com>
2023-05-09 14:47:52 -0300

    adicionado main.c

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+   printf("Bem vindo à Jam Adventure\n");
+   printf("Esta é uma aventura por João Gabriel Macedo e Matheus Lobo\n");
+   printf("\nAproveite nosso jogo e divirta-se!\n");
+   return 0;
+}
